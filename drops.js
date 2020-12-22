b63e0869acd5b069a89dc08bdd0ac63c73092f4c
ArshiaNarang <70150424+ArshiaNarang@users.noreply.github.com>
2020-12-22 20:38:04 +0530

    Add files via upload

---
+++ drops.js
@@ -0,0 +1,22 @@
+class Drops {
+    constructor(x,y) {
+      var options = {
+          friction:0.001,
+          restitution:0.1
+      }
+      this.body = Bodies.circle(x,y,5,options);
+      this.radius = 5;
+      World.add(world, this.body);
+    }
+    update()
+{
+    if(this.body.position.y>height){
+        Matter.Body.setPosition(this.body,{x:random(0,400),y:random(0,400)})
+    }
+}    showDrops(){
+      var pos =this.body.position;
+      ellipseMode(RADIUS);
+      fill("blue");
+      ellipse(pos.x, pos.y, 5,5);
+    }
+  };
